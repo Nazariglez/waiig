aacc877aaa71d3b416990989eab680422a810701
Nazarí González <nazari.nz@gmail.com>
2017-08-16 19:35:35 +0100

    added a simple repl to make easy test in the future

---
+++ main.go
@@ -0,0 +1,21 @@
+// Created by nazarigonzalez on 16/8/17.
+
+package main
+
+import (
+  "fmt"
+  "os"
+  "os/user"
+  "waiig/repl"
+)
+
+func main() {
+  u, err := user.Current()
+  if err != nil {
+    panic(err)
+  }
+
+  fmt.Printf("Hello %s!\n", u.Username)
+  fmt.Printf("Feel free to type in commands\n")
+  repl.Start(os.Stdin, os.Stdout)
+}
